d1a0efeddd21881f0a793c4322a92e069566ef00
Sergey <slisoviy@gmail.com>
2018-11-06 21:56:34 -0700

    Подправил главное меню, добавил background-position в index page

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
-var navMain = document.querySelector('.main-nav__container');
+var navMain = document.querySelector('.main-nav__wrapper');
 var navToggle = document.querySelector('.main-nav__toggle');
+
+var navBgPosition = document.querySelector('.main-nav');
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--closed');
@@ -8,10 +10,10 @@ navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--open');
-    navFill.classList.add('page-header--menu-open');
+    navBgPosition.classList.add('bg-position');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--open');
-    navFill.classList.remove('page-header--menu-open');
+    navBgPosition.classList.remove('bg-position');
   }
 });
